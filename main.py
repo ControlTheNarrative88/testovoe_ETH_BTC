6bba50b5f6d7b4eaafc0c5efc0b0089c7f390cd4
Andrey Elezov <113543774+ControlTheNarrative88@users.noreply.github.com>
2023-05-28 23:44:59 +0300

    Modify main.py

--- main.py
+++ main.py
@@ -34,10 +34,16 @@ def calculate_change(symbol):
     #загружаем цену 
     data = Coin.get_coin_data(symbol)
 
-    max_price = data.max()
-    min_price = data.min()
+    #если определение конкретного времени изменение цены не важно по условию, можно сравнить 
+    #максимальное и минимальное значение чтобы не перебирвать весь датафрейм 
+    max_price = data[symbol].max()
+    min_price = data[symbol].min()
 
     price_change = (max_price - min_price) / min_price
+
+    #используем R-squared из AccessmentModel чтобы вычислить скорректированное изменения цены ETHUSDT
+    #если R-squared = 0.69, то 69% изменений цена ETHUSDT может быть вызвано изменениями цена BTCUSDT
+    #скорректированное изменение цены ETH = желаемое "очищенное" изменение(1%) / 1 - R-squared
     
     if price_change >= (0.01 / (1 - AccessmentModel.r_squared)):
         return True
@@ -45,27 +51,25 @@ def calculate_change(symbol):
     return False
 
 
-    #используем R-squared из AccessmentModel чтобы вычислить скорректированное изменения цены ETHUSDT
-    #если R-squared = 0.69, то 69% изменений цена ETHUSDT может быть вызвано изменениями цена BTCUSDT
-    #скорректированное изменение цены ETH = желаемое "очищенное" изменение(1%) / 1 - R-squared
-
-    return significant_changes.empty
+    
 
 
-# прогоняем функцию по первичному датафрейму, далее в цикле каждую минуту по созданному новому датафрейму
+
+
 def monitor_changes(symbol, interval=60):
-    #df = Coin.get_coin_data(symbol)
 
+    # прогоняем функцию по первичному датафрейму
     if calculate_change(symbol):
-        print("За час цена не менялась(на 1%+)")
-    else:
         print("Цена менялась более чем на 1% за прошедший час")
+    else:
+        print("За час цена не менялась(на 1%+)")
 
+    #далее в цикле каждую минуту по созданному новому датафрейму
     while True:
 
         time.sleep(interval)
 
-        if not calculate_change(symbol):
+        if calculate_change(symbol):
             print("Цена изменилась более чем на 1%")
         print("За час цена не менялась(на 1%+)")
 
